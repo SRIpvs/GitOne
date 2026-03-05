4b6c3c3b528b0e0ba41124a17ada0b92e4649aea
SRIpvs <sriraminfo28@gmail.com>
2026-03-05 11:01:00 +0530

    Changed the name to parthiban

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome Sriram!");
+        System.out.printf("Hello and welcome Parthiban!");
         System.out.println("Learning Git is fun!");
 
         for (int i = 1; i <= 5; i++) {
